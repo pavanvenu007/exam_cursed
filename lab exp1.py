4177d62f94203b132593502301366bf23df6fbed
panda <panda>
2025-12-11 22:16:23 +0530

    shortened programs but cursed

--- lab exp1.py
+++ lab exp1.py
@@ -1,3 +1,6 @@
 n = int(input("enter no. : "))
 f = lambda s, x, i : (print(i), s(s, x // i, i)) if x % i == 0 else (s(s, x, i + 1) if i <= x else None)
 f(f, n, 2)
+
+n = int(input("Enter a number: "))
+[print(i) for i in range(2, n) if n % i == 0]
