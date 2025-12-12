0e7cac8e816cbda4e6640082679f0835d5168962
panda <panda>
2025-12-12 08:06:28 +0530

    shortened programs but cursed

--- lab exp2.py
+++ lab exp2.py
@@ -1,4 +1,10 @@
+#1st
 p = input("Password : ")
 d = u = s = 0
 [(d := 1) if '0' <= x <= '9' else [(u := 1) if 'A' <= x <= 'Z' else [(s := 1) if x in "!@#$%^&*./" else None]] for x in p]
 print("Valid" if (len(p) >= 8 and d and u and s) else "Invalid")
+
+#2nd
+p = input("Password: ")
+valid = len(p) >= 8 and any(x.isdigit() for x in p) and any(x.isupper() for x in p) and any(x in "!@#$%^&*./" for x in p)
+print("Valid" if valid else "Invalid")
